c81df22c32dd48519c766cf0456bf1a7abca456c
Anton Gorshkov <antongorshkov@Antons-MacBook-Pro.local>
2020-12-16 21:50:43 +0200

    radiobuttonLabels init put into one cycle with the method name init

--- src/main/java/UI.java
+++ src/main/java/UI.java
@@ -24,10 +24,12 @@ public class UI extends Application {
         RadioButton[] radioButtons = new RadioButton[BUTTON_NAMES.length];
         VBox vBox = new VBox();
         ToggleGroup radioGroup = new ToggleGroup();
+        TextField[] radiobuttonLabels = new TextField[BUTTON_NAMES.length];
         for (int i = 0; i < BUTTON_NAMES.length; i++) {
             radioButtons[i] = new RadioButton(BUTTON_NAMES[i]);
             radioButtons[i].setToggleGroup(radioGroup);
             vBox.getChildren().add(radioButtons[i]);
+            radiobuttonLabels[i] = new TextField(BUTTON_NAMES[i]);
             final int finalI = i;
             radioButtons[i].setOnAction(new EventHandler<ActionEvent>() {
                 @Override
@@ -35,29 +37,17 @@ public class UI extends Application {
                     BUTTON_NAMES[finalI] = HttpClient.getMethod();
                 }
             });
+            grid.add(radioButtons[i], 0,i);
+            grid.add(radiobuttonLabels[i], 1, i);
         }
         radioButtons[0].setSelected(true);
-        TextField[] radiobuttonLabels = new TextField[BUTTON_NAMES.length];
         vBox.getChildren().add(grid);
-//        for (int i = 0; i < BUTTON_NAMES.length; i++) {
-            grid.add(radioButtons[0], 0,0);
-            radiobuttonLabels[0] = new TextField(BUTTON_NAMES[0]);
-            grid.add(radiobuttonLabels[0], 1, 0);
-
-            grid.add(radioButtons[1], 0,1);
-            radiobuttonLabels[1] = new TextField(BUTTON_NAMES[1]);
-            grid.add(radiobuttonLabels[1], 1, 1);
-
-            grid.add(radioButtons[2], 0,2);
-            radiobuttonLabels[2] = new TextField(BUTTON_NAMES[2]);
-            grid.add(radiobuttonLabels[2], 1, 2);
-//        }
 
         // Hostname Label, Field
         Label hostnameLabel = new Label("request:");
-        grid.add(hostnameLabel, 0, 3);
         TextField hostnameTextField = new TextField("google.com");
         final CharSequence hostname = hostnameTextField.getCharacters();
+        grid.add(hostnameLabel, 0, 3);
         grid.add(hostnameTextField, 1, 3);
 
         // TODO Create Label with the current Request
@@ -65,8 +55,8 @@ public class UI extends Application {
 
         // Response Label, Label
         Label responseLabel = new Label("response:");
-        grid.add(responseLabel, 0, 4);
         final Label responseTextField = new Label("no response yet");
+        grid.add(responseLabel, 0, 4);
         grid.add(responseTextField, 1, 4);
 
         // Button
@@ -74,7 +64,6 @@ public class UI extends Application {
         HBox hbBtn = new HBox(10);
         hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
         hbBtn.getChildren().add(btn);
-        grid.add(hbBtn, 1, 5);
         btn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent e) {
@@ -82,6 +71,7 @@ public class UI extends Application {
                 responseTextField.setText(HttpClient.getResponse());
             }
         });
+        grid.add(hbBtn, 1, 5);
 
 
         Scene scene = new Scene(vBox, 200, 100);
